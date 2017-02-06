1ffc3254214cb60ab78d6b3c25fa77846a252016
Anton Stepan <info@antonstepan.it>
2017-02-06 12:06:10 +0100

    Update index.js

--- src/views/app/index.js
+++ src/views/app/index.js
@@ -9,13 +9,15 @@ import './app.scss';
 const App = ({user, children, logout}) => {
   return (
     <div>
-      <Header
-        user={user}
-        logout={logout}
-      />
-      <main className="main">{children}</main>
+      <div>
+        <Header
+          user={user}
+          logout={logout}
+        />
+        <main className="main">{children}</main>
+      </div>
       <footer>(c) 2017 Anton Stepan - https://github.com/asndev/how-much-coffee-tracker</footer>
-    </div>
+    <div>
   );
 };
 
